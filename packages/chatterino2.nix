a4d94138fe76f170d0b89219c060ed1ea5b5d6db
Bartosz Wojno <bartoszwjn@gmail.com>
2023-04-16 13:39:33 +0200

    Refactor `chatterino2.nix`

--- packages/chatterino2.nix
+++ packages/chatterino2.nix
@@ -12,17 +12,22 @@
   qtmultimedia,
   qtsvg,
   qttools,
-}:
-mkDerivation rec {
+}: let
   pname = "chatterino2";
   version = "2023-03-28";
-  src = fetchFromGitHub {
-    owner = "Chatterino";
-    repo = pname;
-    rev = "7aaec5f5c6a47f4ebaa21aa1c9df4d740b89ab30";
-    hash = "sha256-Eauiq7juDQSEU/oZxWj+K4pqlJF9yslDz5yn3ylejeo=";
-    fetchSubmodules = true;
-  };
-  nativeBuildInputs = [pkg-config cmake];
-  buildInputs = [boost libsecret openssl qtbase qtimageformats qtmultimedia qtsvg qttools];
-}
+  rev = "7aaec5f5c6a47f4ebaa21aa1c9df4d740b89ab30";
+  hash = "sha256-Eauiq7juDQSEU/oZxWj+K4pqlJF9yslDz5yn3ylejeo=";
+in
+  mkDerivation {
+    inherit pname version;
+    src = fetchFromGitHub {
+      owner = "Chatterino";
+      repo = pname;
+      inherit rev hash;
+      fetchSubmodules = true;
+      # use the same store path as `nix-prefetch-github`
+      name = "${pname}-${builtins.substring 0 7 rev}";
+    };
+    nativeBuildInputs = [pkg-config cmake];
+    buildInputs = [boost libsecret openssl qtbase qtimageformats qtmultimedia qtsvg qttools];
+  }
