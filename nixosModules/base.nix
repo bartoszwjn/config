c6dae9d5426c60f7ffd228bd5daca0da1a8cf6fc
Bartosz Wojno <bartoszwjn@gmail.com>
2023-04-17 14:45:21 +0200

    Auto-optimise nix store

--- nixosModules/base.nix
+++ nixosModules/base.nix
@@ -24,6 +24,7 @@
         flake = flakeInputs.nixpkgs;
       };
       settings = {
+        auto-optimise-store = true;
         experimental-features = ["nix-command" "flakes"];
         keep-derivations = true;
         keep-outputs = true;
