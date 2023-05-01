550fbfc6fae29820606c213d6ab3c2c723715a30
Bartosz Wojno <bartoszwjn@gmail.com>
2023-05-01 23:06:46 +0200

    Use systemd-networkd on `grey`

--- nixosConfigurations/grey/network.nix
+++ nixosConfigurations/grey/network.nix
@@ -6,7 +6,7 @@
 }: {
   networking = {
     hostName = "grey";
-    useDHCP = true;
+    dhcpcd.enable = false;
     firewall.enable = true;
     wireless = {
       enable = true;
@@ -15,7 +15,29 @@
     };
   };
 
-  services.tailscale.enable = true;
+  systemd.network = {
+    enable = true;
+    networks = {
+      "10-wired" = {
+        matchConfig.Name = "enp3s0";
+        networkConfig.DHCP = "yes";
+        dhcpV4Config.RouteMetric = 10;
+        ipv6AcceptRAConfig.RouteMetric = 10;
+      };
+      "11-wireless" = {
+        matchConfig.Name = "wlp5s0";
+        networkConfig.DHCP = "yes";
+        dhcpV4Config.RouteMetric = 20;
+        ipv6AcceptRAConfig.RouteMetric = 20;
+      };
+    };
+    wait-online.anyInterface = true;
+  };
+
+  services = {
+    resolved.enable = true;
+    tailscale.enable = true;
+  };
   # otherwise tailscale gets stuck on shutdown trying to send logs
   systemd.services.tailscaled = {
     after = ["nss-lookup.target" "network-online.target"];
