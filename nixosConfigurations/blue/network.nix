a6f5282fb617767c09248f498f5426354e448b2d
Bartosz Wojno <bartoszwjn@gmail.com>
2023-05-04 21:52:37 +0200

    Use systemd-networkd on `blue`

--- nixosConfigurations/blue/network.nix
+++ nixosConfigurations/blue/network.nix
@@ -6,11 +6,25 @@
 }: {
   networking = {
     hostName = "blue";
-    useDHCP = true;
+    dhcpcd.enable = false;
     firewall.enable = true;
   };
 
-  services.tailscale.enable = true;
+  systemd.network = {
+    enable = true;
+    networks = {
+      "10-wired" = {
+        matchConfig.Name = "enp5s0";
+        networkConfig.DHCP = "yes";
+      };
+    };
+    wait-online.anyInterface = true;
+  };
+
+  services = {
+    resolved.enable = true;
+    tailscale.enable = true;
+  };
   # otherwise tailscale gets stuck on shutdown trying to send logs
   systemd.services.tailscaled = {
     after = ["nss-lookup.target" "network-online.target"];
