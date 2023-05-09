0593015343be657edf34aca68947f38b409a309a
Bartosz Wojno <bartoszwjn@gmail.com>
2023-05-09 11:06:11 +0200

    Set `vim` as the default `EDITOR`

--- nixosModules/base.nix
+++ nixosModules/base.nix
@@ -40,25 +40,28 @@ in {
       };
     };
 
-    environment.systemPackages = builtins.attrValues {
-      inherit
-        (pkgs)
-        file
-        git
-        htop
-        hwinfo
-        iftop
-        inetutils
-        iotop-c
-        ldns
-        lshw
-        lsof
-        ntfs3g
-        openssh
-        pciutils
-        vim
-        ;
-      inherit (config.boot.kernelPackages) cpupower;
+    environment = {
+      systemPackages = builtins.attrValues {
+        inherit
+          (pkgs)
+          file
+          git
+          htop
+          hwinfo
+          iftop
+          inetutils
+          iotop-c
+          ldns
+          lshw
+          lsof
+          ntfs3g
+          openssh
+          pciutils
+          vim
+          ;
+        inherit (config.boot.kernelPackages) cpupower;
+      };
+      variables.EDITOR = "vim";
     };
 
     boot.kernel.sysctl."kernel.task_delayacct" = true; # required by iotop-c
