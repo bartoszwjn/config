0f24334f6a317501a96424c604260a296078cc3a
Bartosz Wojno <bartoszwjn@gmail.com>
2023-07-15 15:35:51 +0200

    Switch `blue` from GRUB to systemd-boot

--- nixosConfigurations/blue/boot.nix
+++ nixosConfigurations/blue/boot.nix
@@ -20,37 +20,10 @@
     kernelModules = ["kvm-amd"];
 
     loader = {
-      systemd-boot.enable = false;
-      grub = {
+      systemd-boot = {
         enable = true;
-        enableCryptodisk = true;
-        device = "nodev";
-        efiSupport = true;
-        efiInstallAsRemovable = true;
-        extraEntries = let
-          # nix shell nixpkgs#grub2 --command \
-          #   sudo grub-probe --target=hints_string /mnt/EFI/Microsoft/Boot/bootmgfw.efi
-          grubHints = "--hint-bios=hd0,gpt1 --hint-efi=hd0,gpt1 --hint-baremetal=ahci0,gpt1";
-          # filesystem UUID of Windows EFI partition
-          grubFsUuid = "22E5-28FE";
-        in ''
-          menuentry "Windows Boot Manager" --class windows --class os {
-            insmod part_gpt
-            insmod fat
-            insmod chain
-            search --no-floppy --fs-uuid --set=root ${grubHints} ${grubFsUuid}
-            chainloader /EFI/Microsoft/Boot/bootmgfw.efi
-          }
-          menuentry "UEFI Firmware Settings" {
-            fwsetup
-          }
-          menuentry "Reboot" {
-            reboot
-          }
-          menuentry "Shutdown" {
-            halt
-          }
-        '';
+        consoleMode = "max";
+        editor = false;
       };
     };
   };
