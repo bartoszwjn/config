f2f3d27596c1f0de83d9a3c4f0a03f306ffda008
Bartosz Wojno <bartoszwjn@gmail.com>
2023-11-06 01:48:37 +0100

    Emacs: update org directory

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -32,7 +32,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Nextcloud/org/current/")
+(setq org-directory "~/syncthing/org/current/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
