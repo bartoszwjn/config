2633be55b32364699e4c700aa3bfafed356e1a04
Bartosz Wojno <bartoszwjn@gmail.com>
2023-11-27 18:25:20 +0100

    Add bash 3.2 package

---
+++ packages/bash_3_2.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  bison,
+}: let
+  pname = "bash";
+  version = "3.2.57";
+  hash = "sha256-P6na+F6/NQaPCQzlEoPd7rPHXrW8cLGkp8sFhov+BqQ=";
+in
+  stdenv.mkDerivation {
+    inherit pname version;
+
+    src = fetchurl {
+      url = "mirror://gnu/bash/bash-${version}.tar.gz";
+      inherit hash;
+    };
+
+    buildInputs = [bison];
+
+    env.NIX_CFLAGS_COMPILE = ''
+      -Wno-error=format-security
+    '';
+
+    meta = {
+      license = lib.licenses.gpl2Only;
+    };
+  }
--- packages/default.nix
+++ packages/default.nix
@@ -1,3 +1,4 @@
 {pkgs}: {
+  bash_3_2 = pkgs.callPackage ./bash_3_2.nix {};
   chatterino2 = pkgs.libsForQt5.callPackage ./chatterino2.nix {};
 }
