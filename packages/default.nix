201fd65979dc58d2473b16dc98af573418812ccb
Bartosz Wojno <bartoszwjn@gmail.com>
2024-01-11 22:32:03 +0100

    Add `packages/chatterino2` directory

--- packages/default.nix
+++ packages/default.nix
@@ -1,5 +1,5 @@
 {pkgs}: {
   bash_3_2 = pkgs.callPackage ./bash_3_2.nix {};
-  chatterino2 = pkgs.libsForQt5.callPackage ./chatterino2.nix {};
+  chatterino2 = pkgs.libsForQt5.callPackage ./chatterino2 {};
   neovim-custom = pkgs.callPackage ./neovim-custom.nix {};
 }
