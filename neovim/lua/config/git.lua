68c72d40c1a6f9b3263279df2dc71a40e9c2cc3a
Bartosz Wojno <bartoszwjn@gmail.com>
2024-02-21 02:34:32 +0100

    nvim: add keymaps for diffview

--- neovim/lua/config/git.lua
+++ neovim/lua/config/git.lua
@@ -20,4 +20,10 @@ local function nmap(lhs, description, rhs)
 end
 
 nmap("<Leader>g", "[g]it", "<Nop>")
-nmap("<Leader>gg", "[g]it: neo[g]it status", neogit.open)
+nmap("<Leader>gd", "[g]it: open [d]iffview", diffview.open)
+nmap("<Leader>gD", "[g]it: close [D]iffview", diffview.close)
+nmap("<Leader>gs", "[g]it: neogit [s]tatus", neogit.open)
+nmap("<Leader>gh", "[g]it: current file [h]istory", function()
+  diffview.file_history(nil, { vim.fn.expand("%") })
+end)
+nmap("<Leader>gH", "[g]it: DiffviewFile[H]istory", ":DiffviewFileHistory ")
