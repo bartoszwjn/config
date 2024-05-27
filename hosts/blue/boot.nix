44b9b6030b847fa79204c0f109ff7e1d79273db5
Bartosz Wojno <bartoszwjn@gmail.com>
2024-05-27 02:58:49 +0200

    blue: add more storage

--- hosts/blue/boot.nix
+++ hosts/blue/boot.nix
@@ -10,10 +10,25 @@
     initrd = {
       availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "sd_mod"];
       kernelModules = ["dm-snapshot"];
-      luks.devices."root" = {
-        device = "/dev/vg0/cryptroot";
-        preLVM = false;
-        allowDiscards = true;
+      luks = {
+        reusePassphrases = true;
+        devices = {
+          root = {
+            device = "/dev/vg0/cryptroot";
+            preLVM = false;
+            allowDiscards = true;
+          };
+          data-0-0 = {
+            device = "/dev/disk/by-uuid/6bfa567a-0adc-4332-8849-41dadfcf465c";
+            preLVM = false;
+            allowDiscards = true;
+          };
+          data-0-1 = {
+            device = "/dev/disk/by-uuid/9174ee64-8fe1-47a4-87af-94b597560e4d";
+            preLVM = false;
+            allowDiscards = true;
+          };
+        };
       };
     };
 
@@ -35,6 +50,10 @@
     "/boot" = {
       device = "/dev/disk/by-uuid/7D58-3055";
       fsType = "vfat";
+    };
+    "/mnt/data-0" = {
+      device = "/dev/disk/by-label/data-0";
+      fsType = "btrfs";
     };
   };
 
