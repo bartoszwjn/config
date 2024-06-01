ba8e4e2e94fc1ad71793d8d8daa3d52858de937d
Bartosz Wojno <bartoszwjn@gmail.com>
2024-06-01 23:13:19 +0200

    t824: update Hyprland monitor config

--- hosts/t824/homes/bart3.nix
+++ hosts/t824/homes/bart3.nix
@@ -33,11 +33,12 @@ in {
     hyprland = {
       enable = true;
       monitorsConfig = [
-        # TODO eDP-1?
+        "eDP-2, 2560x1600@120, 0x0, 1.33"
         "desc:Dell Inc. DELL S2522HG GQ6L1C3, 1920x1080@120, -1920x0, 1"
+        "desc:LG Electronics LG Ultra HD 0x00049402, 1920x1080@60, -1920x0, 1"
         ", preferred, auto, 1"
       ];
-      waybar.monitors = ["eDP-1" "HDMI-A-1"]; # TODO
+      waybar.monitors = ["eDP-2" "HDMI-A-1"];
       waybar.showBattery = true;
     };
     neovim.enable = true;
