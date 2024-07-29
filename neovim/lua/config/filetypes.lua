5a677bcbafec63a091f68f360a1b0888f560dd2f
Bartosz Wojno <bartoszwjn@gmail.com>
2024-07-29 19:09:24 +0200

    nvim: set `ft=groovy` in Jenkinsfiles

---
+++ neovim/lua/config/filetypes.lua
@@ -0,0 +1,8 @@
+local ft_detect_group = vim.api.nvim_create_augroup("ft_detect_custom", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = ft_detect_group,
+  pattern = "*.Jenkinsfile",
+  desc = "use ft=groovy in Jenkinsfiles",
+  callback = function() vim.bo.filetype = "groovy" end,
+})
