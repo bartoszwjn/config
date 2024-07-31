1b4ed96634cae72799380fee7761eafb5b605101
Bartosz Wojno <bartoszwjn@gmail.com>
2024-07-31 14:47:42 +0200

    nvim: format Nix code using nixfmt

--- neovim/ftplugin/nix.lua
+++ neovim/ftplugin/nix.lua
@@ -4,7 +4,12 @@ local function nmap(lhs, description, rhs)
   vim.keymap.set("n", lhs, rhs, { buffer = true, desc = description })
 end
 
-nmap("<Leader>cf", "[c]ode [f]ormat (Nix)", function()
+nmap("<Leader>cf", "[c]ode [f]ormat (Nix): nixfmt", function()
+  vim.cmd.update()
+  vim.cmd("!nixfmt --width=" .. vim.o.textwidth .. " %")
+end)
+
+nmap("<Leader>cF", "[c]ode [F]ormat (Nix): alejandra", function()
   vim.cmd.update()
   vim.cmd("!alejandra --quiet %")
 end)
