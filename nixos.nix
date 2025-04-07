68957003c938d1bba2e7f1d8dc77f9e555c89fd1
Bartosz Wojno <bartoszwjn@gmail.com>
2025-04-07 22:44:50 +0200

    nixos: customize nixos output attributes

--- nixos.nix
+++ nixos.nix
@@ -14,27 +14,33 @@
 }:
 let
   privateConfig = import private-config;
-in
-import (nixpkgs + "/nixos/lib/eval-config.nix") {
-  system = null;
-  inherit lib;
-  modules = [
-    ./hosts/${name}/configuration.nix
-    (home-manager + "/nixos")
-    (sops-nix + "/modules/sops")
-    (disko + "/module.nix")
-    {
-      _module.args = { inherit customPkgs privateConfig; };
-      nixpkgs.pkgs = pkgs;
-      nix.registry = {
-        home-manager.flake = home-manager;
-        nixpkgs.flake = nixpkgs;
-      };
 
-      home-manager.sharedModules = [
-        (sops-nix + "/modules/home-manager/sops.nix")
-        { _module.args = { inherit customPkgs privateConfig; }; }
-      ];
-    }
-  ] ++ lib.optional readOnlyPkgs (nixpkgs + "/nixos/modules/misc/nixpkgs/read-only.nix");
+  eval = import (nixpkgs + "/nixos/lib/eval-config.nix") {
+    system = null;
+    inherit lib;
+    modules = [
+      ./hosts/${name}/configuration.nix
+      (home-manager + "/nixos")
+      (sops-nix + "/modules/sops")
+      (disko + "/module.nix")
+      {
+        _module.args = { inherit customPkgs privateConfig; };
+        nixpkgs.pkgs = pkgs;
+        nix.registry = {
+          home-manager.flake = home-manager;
+          nixpkgs.flake = nixpkgs;
+        };
+
+        home-manager.sharedModules = [
+          (sops-nix + "/modules/home-manager/sops.nix")
+          { _module.args = { inherit customPkgs privateConfig; }; }
+        ];
+      }
+    ] ++ lib.optional readOnlyPkgs (nixpkgs + "/nixos/modules/misc/nixpkgs/read-only.nix");
+  };
+in
+{
+  inherit eval;
+  inherit (eval) pkgs config options;
+  system = eval.config.system.build.toplevel;
 }
