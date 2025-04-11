d30f227e174b1707009968159d117ed5a7559489
Bartosz Wojno <bartoszwjn@gmail.com>
2025-04-11 11:24:12 +0200

    Check flake.lock version when fetching inputs manually

--- inputs.nix
+++ inputs.nix
@@ -1,6 +1,7 @@
 let
   lock = builtins.fromJSON (builtins.readFile ./flake.lock);
 in
+assert lock.version == 7;
 builtins.mapAttrs (
   inputName: nodeName: builtins.fetchTree lock.nodes.${nodeName}.locked
 ) lock.nodes.${lock.root}.inputs
