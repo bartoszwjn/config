22b1b9be9f07c37bae4ddb65d66365eab68d684c
Bartosz Wojno <bartoszwjn@gmail.com>
2025-07-26 16:04:35 +0200

    ndf: WIP get drv paths

--- packages/ndf/src/main.rs
+++ packages/ndf/src/main.rs
@@ -1,11 +1,14 @@
-use std::process::ExitCode;
+use std::{
+    collections::{BTreeSet, HashMap},
+    process::ExitCode,
+};
 
 use anstream::{eprintln, println};
 use clap::Parser as _;
 
-use cli::Cli;
-use color::RED_BOLD;
-use items::ItemPair;
+use cli::{Cli, DiffProgram};
+use color::{GREEN_BOLD, RED_BOLD};
+use items::{GitRev, Item, ItemPair, SourceType};
 
 mod cli;
 mod color;
@@ -26,11 +29,58 @@ fn main() -> ExitCode {
 }
 
 fn run(args: Cli) -> anyhow::Result<()> {
+    let program = args.program;
     let items = ItemPair::from_args(args)?;
 
-    for pair in items.iter() {
+    for pair in &items {
         println!("{}", pair);
     }
+    println!();
 
-    todo!("diff")
+    let drv_paths = eval_drv_paths(&items)?;
+
+    for (pair, (old_path, new_path)) in items.iter().zip(&drv_paths) {
+        println!("{RED_BOLD}-{RED_BOLD:#} {} {}", old_path, pair.old);
+        println!("{GREEN_BOLD}+{GREEN_BOLD:#} {} {}", new_path, pair.new);
+        match program {
+            DiffProgram::None => {}
+            DiffProgram::NixDiff => {
+                todo!("nix-diff diff")
+            }
+            DiffProgram::Nvd => todo!("nvd diff"),
+        }
+    }
+
+    todo!("summary");
+}
+
+fn eval_drv_paths(items: &[ItemPair]) -> anyhow::Result<Vec<(String, String)>> {
+    let eval_jobs = {
+        let mut map = HashMap::<(&SourceType, &GitRev), BTreeSet<&str>>::new();
+        for item in items.iter().flat_map(|pair| [&pair.old, &pair.new]) {
+            map.entry((&item.source, &item.git_rev))
+                .or_default()
+                .insert(&item.attr_path);
+        }
+        map
+    };
+
+    let drv_paths = {
+        let mut map = HashMap::<(&SourceType, &GitRev, &str), String>::new();
+        for ((source, rev), attr_paths) in eval_jobs {
+            let attr_paths = attr_paths.into_iter().collect::<Vec<_>>();
+            let drv_paths = nix::get_drv_paths(source, rev, &attr_paths)?;
+            for (attr_path, drv_path) in attr_paths.into_iter().zip(drv_paths) {
+                map.insert((source, rev, attr_path), drv_path);
+            }
+        }
+        map
+    };
+
+    let get_drv_path =
+        |item: &Item| drv_paths[&(&item.source, &item.git_rev, item.attr_path.as_str())].clone();
+    Ok(items
+        .iter()
+        .map(|pair| (get_drv_path(&pair.old), get_drv_path(&pair.new)))
+        .collect())
 }
--- packages/ndf/src/nix.rs
+++ packages/ndf/src/nix.rs
@@ -1,6 +1,9 @@
 use std::{path::Path, process::Command};
 
-use crate::command;
+use crate::{
+    command,
+    items::{GitRev, SourceType},
+};
 
 fn get_current_system() -> anyhow::Result<String> {
     command::run_json(
@@ -36,4 +39,94 @@ pub(crate) fn get_file_output_attributes(file: &Path) -> anyhow::Result<Vec<Stri
                 "x: let r = if builtins.isFunction x then x {} else x; in builtins.attrNames r",
             ]),
     )
+}
+
+pub(crate) fn get_drv_paths(
+    source: &SourceType,
+    rev: &GitRev,
+    attr_paths: &[&str],
+) -> anyhow::Result<Vec<String>> {
+    let result = match source {
+        SourceType::FlakeCurrentDir => {
+            let current_system = get_current_system()?;
+            let mut cmd = Command::new("nix");
+            cmd.args([
+                "eval",
+                "--json",
+                "--apply",
+                &get_flake_drv_paths_expr(attr_paths, &current_system),
+            ]);
+            let flake_ref = match rev {
+                GitRev::Rev { rev, .. } => &format!(".?rev={}#.", rev),
+                GitRev::Worktree => ".#.",
+            };
+            cmd.args(["--", flake_ref]);
+            command::output_json(&mut cmd)?
+        }
+        SourceType::File(path) => {
+            // Without at least one `-A` `nix-instantiate --eval` doesn't auto-call functions
+            if attr_paths.is_empty() {
+                vec![]
+            } else {
+                let mut cmd = Command::new("nix-instantiate");
+                cmd.args(["--eval", "--strict", "--json"]);
+                for &attr_path in attr_paths {
+                    cmd.args(["--attr", &format!("{}.drvPath", attr_path)]);
+                }
+                match rev {
+                    GitRev::Worktree => {
+                        cmd.arg("--");
+                        cmd.arg(path);
+                    }
+                    GitRev::Rev { rev, .. } => {
+                        cmd.args([
+                            "--expr",
+                            // TODO find git repo root, import file relative to repo root
+                            r#"{path, rev}: builtins.fetchGit { url = path; inherit rev; name = "source"; }"#,
+                        ]);
+                        cmd.args(["--arg", "path"]);
+                        cmd.arg(path);
+                        cmd.args(["--argstr", "rev", rev]);
+                    }
+                }
+                command::output_json_lines(&mut cmd)?
+            }
+        }
+    };
+
+    assert_eq!(result.len(), attr_paths.len());
+    Ok(result)
+}
+
+fn get_flake_drv_paths_expr(attr_paths: &[&str], current_system: &str) -> String {
+    let mut expr = String::from("flake: [");
+    let mut first = true;
+    for &attr_path in attr_paths {
+        if first {
+            first = false;
+        } else {
+            expr.push(' ');
+        }
+        expr.push('(');
+        let candidates = [
+            format!("packages.{current_system}."),
+            format!("legacyPackages.{current_system}."),
+            String::new(),
+        ]
+        .into_iter()
+        .map(|prefix| prefix + attr_path)
+        .collect::<Vec<_>>();
+        for candidate in &candidates {
+            expr.push_str(&format!(
+                "if flake ? {candidate} then flake.{candidate} else "
+            ));
+        }
+        expr.push_str(&format!(
+            r#"throw "flake does not provide attribute '{}', '{}' or '{}'""#,
+            candidates[0], candidates[1], candidates[2],
+        ));
+        expr.push(')');
+    }
+    expr.push(']');
+    expr
 }
