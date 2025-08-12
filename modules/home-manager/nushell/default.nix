983de08d34b750568ceeca4d12bd24f4b3690b55
Bartosz Wojno <bartoszwjn@gmail.com>
2025-08-12 13:38:53 +0200

    nushell: configure zoxide

--- modules/home-manager/nushell/default.nix
+++ modules/home-manager/nushell/default.nix
@@ -18,7 +18,16 @@ in
     home.packages = [ pkgs.nushell ];
 
     xdg.configFile = {
-      "nushell/autoload".source = ./autoload;
+      "nushell/autoload" = {
+        source = ./autoload;
+        recursive = true;
+      };
+      "nushell/autoload/zoxide.nu".source =
+        pkgs.runCommand "zoxide-nushell-config.nu"
+          { nativeBuildInputs = [ config.programs.zoxide.package ]; }
+          ''
+            zoxide init nushell ${lib.escapeShellArgs config.programs.zoxide.options} > "$out"
+          '';
       "nushell/config.nu".source = ./config.nu;
       "nushell/env.nu".source = ./env.nu;
     };
