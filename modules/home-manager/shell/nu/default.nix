3c404b70f99d8280b2fd6f874c323e51f3c08008
Bartosz Wojno <bartoszwjn@gmail.com>
2025-10-29 13:39:51 +0100

    shell/nu: install formats plugin

--- modules/home-manager/shell/nu/default.nix
+++ modules/home-manager/shell/nu/default.nix
@@ -7,6 +7,10 @@
 let
   inherit (lib) types;
   cfg = config.custom.shell.nu;
+
+  plugins = lib.attrValues {
+    inherit (pkgs.nushellPlugins) formats;
+  };
 in
 {
   options.custom.shell.nu = {
@@ -32,6 +36,33 @@ in
         };
         "nushell/config.nu".source = ./config.nu;
         "nushell/env.nu".source = ./env.nu;
+        "nushell/plugin.msgpackz".source =
+          let
+            pluginPaths = map lib.getExe plugins;
+
+            # Write all plugin program paths to a file
+            # so that Nix can detect them as runtime dependencies of plugin.msgpackz
+            pluginPathsFile = pkgs.writeText "nushell-plugin-paths.txt" (lib.concatStringsSep "\n" pluginPaths);
+
+            pluginConfig = pkgs.runCommand "nushell-plugins" { nativeBuildInputs = [ pkgs.nushell ]; } ''
+              ${lib.concatMapStrings (
+                pluginPath:
+                let
+                  addCmd = "plugin add --plugin-config ./plugin.msgpackz ${pluginPath}";
+                in
+                ''
+                  nu --no-config-file --no-std-lib --commands ${lib.escapeShellArg addCmd}
+                ''
+              ) pluginPaths}
+
+              mkdir $out
+              mv ./plugin.msgpackz $out/plugin.msgpackz
+
+              mkdir $out/nix-support
+              cp ${pluginPathsFile} $out/nix-support/plugin-paths.txt
+            '';
+          in
+          pluginConfig + "/plugin.msgpackz";
       }
       (lib.mapAttrs' (
         name: path: lib.nameValuePair "nushell/autoload/${name}" { source = path; }
