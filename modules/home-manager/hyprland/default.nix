0725050440e206964d0e69e6d3e03213d3dac863
Bartosz Wojno <bartoszwjn@gmail.com>
2025-12-02 22:52:04 +0100

    Hyprland: update signal window rule

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -179,7 +179,7 @@ in
 
         windowrulev2 = [
           "workspace 5 silent, class:^(Slack)$"
-          "workspace 6 silent, class:^(signal)$"
+          "workspace 6 silent, class:^(?i:signal)$"
           "workspace 7 silent, class:^(obsidian)$"
           "workspace 9 silent, class:^(spotify)$"
           # pavucontrol
