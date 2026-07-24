b09be70ebf11eb004c88b98b533e2118f9d60c4f
Bartosz Wojno <bartoszwjn@gmail.com>
2026-07-24 15:56:29 +0200

    arnold: fix formatting

--- hosts/arnold/network.nix
+++ hosts/arnold/network.nix
@@ -18,8 +18,8 @@
 
   # https://github.com/tailscale/tailscale/issues/11504#issuecomment-2667381779
   systemd.services.tailscale-online = {
-    after = ["tailscaled.service"];
-    bindsTo = ["tailscaled.service"];
+    after = [ "tailscaled.service" ];
+    bindsTo = [ "tailscaled.service" ];
     script = ''
       until ${lib.getExe' config.services.tailscale.package "tailscale"} status --peers=false; do
         sleep 1
